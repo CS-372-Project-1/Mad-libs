d6284a7a3a6605272ef45b6ddf93ff72827bf897
alpshad <38167513+alpshad@users.noreply.github.com>
2021-09-09 16:52:36 -0700

    Refactored replace words

--- mad-libs/src/files.rs
+++ mad-libs/src/files.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::fs;
 use regex::Regex;
+use std::collections::HashMap;
 
 pub fn read_file() -> String {
     // Pick a random number to choose the file
@@ -31,11 +32,25 @@ pub fn parse_words(story:&str) -> Vec<String> {
     return word_vec;
 }
 
-pub fn replace_words(story:&str, words:Vec<String>) -> String {
+pub fn replace_words(story:&str, words:HashMap<String, Vec<String>>) -> String {
     let regex = Regex::new(r"\{[ \w\d_()-]*\}").unwrap();
     let mut result: String = story.to_string();
-    for word in words.iter() {
-        result = regex.replace(&result, word.to_uppercase()).to_string();
+    // for word in words.iter() {
+    //     result = regex.replace(&result, word.to_uppercase()).to_string();
+    // }
+
+    for caps in regex.captures_iter(story) {
+        let mut reg_match = caps.get(0).unwrap().as_str().to_string();
+        reg_match = reg_match.replace("{ ", "");
+        reg_match = reg_match.replace(" }", "");
+        if words.contains_key(&reg_match) {
+            let mut v = words.get_mut(&reg_match).unwrap();
+            let mut generator = rand::thread_rng();
+            let input = generator.gen_range(0..v.len());
+            let element = v.get(input).unwrap();
+            v.remove(input);
+            result = regex.replace(story, element.to_uppercase()).to_string();
+        }
     }
 
     return result;
--- mad-libs/src/main.rs
+++ mad-libs/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 mod files;
 mod game;
 
@@ -14,3 +16,16 @@ fn main() {
 
 // Use hash map: loop through word types, add to map if not seen,
 // otherwise add user input to 
+
+pub fn initializeMap(words:Vec<String>) -> HashMap<String, Vec<String>> {
+    let mut input_map: HashMap<String, Vec<String>> = HashMap::new();
+    
+    for word in words.iter() {
+        if !input_map.contains_key(word) {
+            let empty_vec: Vec<String> = Vec::new();
+            input_map.insert(word.to_string(), empty_vec);
+        }
+    }
+
+    return input_map;
+}
