909dc8056be7bba7830978a0db08412d9ba25f77
alpshad <38167513+alpshad@users.noreply.github.com>
2021-09-09 17:44:01 -0700

    Bug Fixes

--- mad-libs/src/main.rs
+++ mad-libs/src/main.rs
@@ -4,39 +4,51 @@ use std::io;
 mod files;
 mod game;
 
-static mut MAP: HashMap<String, Vec<String>> = HashMap::new();
-
 fn main() {
     println!("Hello, and welcome to Mad Libs!");
     println!("Let's get started!");
-    let story = files::read_file();
-    let words: Vec<String> = game::parse_words(&story);
-    println!("Words: {:?}", words);
-    // Need to get user input here
-    let new_story = game::replace_words(&story, words); // should be called with user input
-    println!("New Story: {}", new_story);
-}
 
-// Use hash map: loop through word types, add to map if not seen,
-// otherwise add user input to 
-
-pub fn initialize_map(words:Vec<String>) {
-    for word in words.iter() {
-        if !MAP.contains_key(word) {
-            let empty_vec: Vec<String> = Vec::new();
-            MAP.insert(word.to_string(), empty_vec);
+    loop {
+        let story = files::read_file();
+        let words: Vec<String> = game::parse_words(&story);
+        println!("Words: {:?}", words);
+        let mut map = get_user_input();
+        let new_story = game::replace_words(&story, map); // should be called with user input
+        println!("New Story: {}", new_story);
+        println!();
+        println!("Would you like to play again? (Y/N)");
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("No input found.");
+        if !user_input.to_uppercase().eq("Y\n") {
+            println!("Thanks for playing!");
+            break;
         }
     }
 }
 
-pub fn fill_map(user_input:Vec<(String, String)>) {
-        for input_group in user_input {
-            let v = MAP.get_mut(&input_group.0).unwrap();
-            v.push(input_group.1);
+pub fn initialize_map(words:Vec<String>) -> HashMap<String, Vec<String>> {
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for word in words.iter() {
+        if !map.contains_key(word) {
+            let empty_vec: Vec<String> = Vec::new();
+            map.insert(word.to_string(), empty_vec);
         }
+    }
+
+    return map;
 }
 
-pub fn get_user_input() {    
+pub fn fill_map(user_input:Vec<(String, String)>, map:HashMap<String, Vec<String>>) -> HashMap<String, Vec<String>> {
+        for input_group in user_input {
+            let v = map.get_mut(&input_group.0).unwrap();
+            v.push(input_group.1);
+        }
+
+        return map;
+}
+
+pub fn get_user_input() -> Vec<(String, String)> {    
     let mut in_str = String::new();
     // loop to fill global hashmap
     io::stdin().read_line(&mut in_str).expect("Error: Please input a word");
--- mad-libs/target/debug/mad-libs.d
+++ mad-libs/target/debug/mad-libs.d
@@ -1,1 +1,1 @@
-/Users/amypaul/Documents/GitHub/Mad-libs/mad-libs/target/debug/mad-libs: /Users/amypaul/Documents/GitHub/Mad-libs/mad-libs/src/files.rs /Users/amypaul/Documents/GitHub/Mad-libs/mad-libs/src/main.rs
+/Users/amypaul/Documents/GitHub/Mad-libs/mad-libs/target/debug/mad-libs: /Users/amypaul/Documents/GitHub/Mad-libs/mad-libs/src/main.rs
