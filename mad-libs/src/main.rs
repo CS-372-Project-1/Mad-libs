7fefd83ad8a2075df3a94a7c647c6221fd6f0bfc
alpshad <38167513+alpshad@users.noreply.github.com>
2021-09-09 18:18:54 -0700

    Finished User Input

--- mad-libs/src/main.rs
+++ mad-libs/src/main.rs
@@ -12,8 +12,8 @@ fn main() {
         let story = files::read_file();
         let words: Vec<String> = game::parse_words(&story);
         println!("Words: {:?}", words);
-        let map = initialize_map(words);
-        let mut matches = get_user_input();
+        let mut map = initialize_map(words);
+        let matches = get_user_input(words);
         let new_story = game::replace_words(&story, fill_map(matches, map)); // should be called with user input
         println!("Your Story: {}", new_story);
         println!();
@@ -40,7 +40,7 @@ pub fn initialize_map(words:Vec<String>) -> HashMap<String, Vec<String>> {
     return map;
 }
 
-pub fn fill_map(user_input:Vec<(String, String)>, map:HashMap<String, Vec<String>>) -> HashMap<String, Vec<String>> {
+pub fn fill_map(user_input:Vec<(String, String)>, map:&mut HashMap<String, Vec<String>>) -> &mut HashMap<String, Vec<String>> {
         for input_group in user_input {
             let v = map.get_mut(&input_group.0).unwrap();
             v.push(input_group.1);
@@ -49,8 +49,16 @@ pub fn fill_map(user_input:Vec<(String, String)>, map:HashMap<String, Vec<String
         return map;
 }
 
-pub fn get_user_input() -> Vec<(String, String)> {    
-    let mut in_str = String::new();
-    // loop to fill global hashmap
-    io::stdin().read_line(&mut in_str).expect("Error: Please input a word");
+pub fn get_user_input(words:Vec<String>) -> Vec<(String, String)> {    
+    let mut v: Vec<(String, String)> = Vec::new();
+
+    for word in words {
+        println!("Enter a {}: ", word);
+        let mut in_str = String::new();
+        io::stdin().read_line(&mut in_str).expect("Error: Please input a word");
+        v.push((word, in_str));
+    }
+
+    return v;
+
 }
