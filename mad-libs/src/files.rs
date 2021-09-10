d8c71ad5be7e560774d52a26206ca5987041a561
alpshad <38167513+alpshad@users.noreply.github.com>
2021-09-09 18:19:52 -0700

    Update files.rs

--- mad-libs/src/files.rs
+++ mad-libs/src/files.rs
@@ -13,30 +13,3 @@ pub fn read_file() -> String {
         .expect("File not found or could not be read");
     return story;
 }
-
-pub fn parse_words(story:&str) -> Vec<String> {
-    // Read tokens ({}) into vector of strings
-    // return vector
-
-    let mut word_vec: Vec<String> = vec![];
-    let regex = Regex::new(r"\{[ \w\d_()-]*\}").unwrap();
-
-    for caps in regex.captures_iter(story) {
-        let mut reg_match = caps.get(0).unwrap().as_str().to_string();
-        reg_match = reg_match.replace("{ ", "");
-        reg_match = reg_match.replace(" }", "");
-        word_vec.push(reg_match);
-    }
-
-    return word_vec;
-}
-
-pub fn replace_words(story:&str, words:Vec<String>) -> String {
-    let regex = Regex::new(r"\{[ \w\d_()-]*\}").unwrap();
-    let mut result: String = story.to_string();
-    for word in words.iter() {
-        result = regex.replace(&result, word.to_uppercase()).to_string();
-    }
-
-    return result;
-}
